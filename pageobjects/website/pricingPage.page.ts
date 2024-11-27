fe0751fb50d15f6f7c33e7b9df345aee7d9c0d7e
n.yeliashkevich <n.yeliashkevich@smartbroker.de>
2024-11-27 23:10:46 +0100

    Implemented login and pricing pages

--- pageobjects/website/pricingPage.page.ts
+++ pageobjects/website/pricingPage.page.ts
@@ -1,16 +1,33 @@
-import { Page } from 'playwright-core'
+import { Page } from "playwright-core";
 
 export default class PricingPage {
-  private page: Page
+  page: Page;
 
   constructor(page: Page) {
-    this.page = page
+    this.page = page;
   }
 
   // Elements
   //-------------------------------------------------------------------------------------------------------------------
-  
+  //Pricing Page
+  readonly getProButton = () =>
+    this.page.getByRole("button", { name: "Get Pro" });
+  readonly getExpertButton = () =>
+    this.page.getByRole("button", { name: "Get Expert" });
+  readonly getFreeButton = () =>
+    this.page.getByRole("button", { name: "Get Free" });
+  readonly freePlanTitle = () => this.page.getByText("Free", { exact: true });
+  readonly proPlanTitle = () => this.page.getByText("Pro", { exact: true });
+  readonly expertPlanTitle = () =>
+    this.page.getByText("Expert", { exact: true });
+  //Subscription Modal
+  readonly proceedToCheckoutButton = () =>
+    this.page.getByText("Proceed to Checkout –>");
+  readonly proPlanSubscriptionModal = () =>
+    this.page.getByRole("heading", { name: "Upgrade to Pro plan" });
   // Actions
   //-------------------------------------------------------------------------------------------------------------------
-  
+  async selectProPlan() {
+    await this.getProButton().click();
+  }
 }
