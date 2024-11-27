5032b7df8f81c4aac7f98e7af5c7e872cef32d2f
n.yeliashkevich <n.yeliashkevich@smartbroker.de>
2024-11-27 23:08:36 +0100

    Added basePage for generic logic

--- fixture.ts
+++ fixture.ts
@@ -1,19 +1,25 @@
-import { test as base } from "@playwright/test";
+import {test as base} from "@playwright/test";
+import BasePage from "./pageobjects/website/basePage.page";
 import PricingPage from "./pageobjects/website/pricingPage.page";
 import LoginModal from "./pageobjects/website/loginModal.page";
 
 const fixtures = base.extend<{
-  pricingPage: PricingPage;
-  loginModal: LoginModal;
+    basePage: BasePage;
+    pricingPage: PricingPage;
+    loginModal: LoginModal;
 }>({
-  pricingPage: async ({ page }, use) => {
-    const pricingPage = new PricingPage(page);
-    await use(pricingPage);
-  },
-  loginModal: async ({ page }, use) => {
-    const loginModal = new LoginModal(page);
-    await use(loginModal);
-  },
+    basePage: async ({page}, use) => {
+        const basePage = new BasePage(page);
+        await use(basePage);
+    },
+    pricingPage: async ({page}, use) => {
+        const pricingPage = new PricingPage(page);
+        await use(pricingPage);
+    },
+    loginModal: async ({page}, use) => {
+        const loginModal = new LoginModal(page);
+        await use(loginModal);
+    },
 });
 
-export { fixtures };
+export {fixtures};
---
+++ pageobjects/website/basePage.page.ts
@@ -0,0 +1,28 @@
+import { Page } from "playwright-core";
+
+export default class BasePage {
+  protected page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  // Elements
+  //-------------------------------------------------------------------------------------------------------------------
+  readonly allowCookiesButton = () =>
+    this.page.getByRole("button", { name: "Allow all" });
+  readonly plansButton = () => this.page.getByRole("button", { name: "Plans" });
+  // Actions
+  //-------------------------------------------------------------------------------------------------------------------
+  async acceptCookies() {
+    await this.allowCookiesButton().click();
+  }
+
+  async navigateTo(path: string) {
+    await this.page.goto(path);
+  }
+
+  async clickPlansButton() {
+    await this.plansButton().click();
+  }
+}
