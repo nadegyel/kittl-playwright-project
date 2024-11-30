eeaeac83831f2272994e7c373134d956e6760f0f
n.yeliashkevich <n.yeliashkevich@smartbroker.de>
2024-11-30 21:46:54 +0100

    Removed CI retries in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   testDir: "./specs",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 0 : 0,
   workers: process.env.CI ? 2 : undefined,
   reporter: [
     ["list"],
